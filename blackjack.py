8241d5276afd8a2134f57410f7037332b5549048
unknown <cgnatke@gmail.com>
2022-05-28 19:03:15 -0400

    first commitc

---
+++ blackjack.py
@@ -0,0 +1,181 @@
+# https://edge.twinspires.com/blackjack-terms-explained-blackjack-glossary-and-terminology/
+# https://bicyclecards.com/how-to-play/blackjack/
+
+from collections import namedtuple
+import random
+
+
+minimum_bet = 10
+
+
+def add_to_bankroll(bankroll: int):
+    print(f"Your bankroll currently is {bankroll}.")
+    return int(input("How much would you like to add to your bankroll? "))
+
+
+def get_bet_amount(bankroll: int):
+    while(True):
+        amt = int(
+            input(f"How much do you want to bet? Table minimum is {minimum_bet}: "))
+        if amt >= minimum_bet:
+            print(f"You are betting {amt}!")
+            if amt == bankroll:
+                print("Holy $^$#@#@! you went all in!!!")
+            return amt  # exit
+        elif amt > bankroll:
+            print(
+                "Try again- You cannot bet more than your current bankroll of {bankroll}")
+        else:
+            print("Try again- You need to bet at least the table minimum...")
+
+
+def get_card_value(card_num: int):
+    if(card_num < 2 or card_num > 14):
+        raise ValueError(
+            "card_num provided out of the range of 2-13, inclusive")
+    elif(card_num < 11):  # cards less than 10
+        return card_num
+    elif card_num < 14:  # face card or 10
+        return 10
+    elif card_num == 14:  # ace
+        return 11
+
+# todo this function should accept a list of cards and add them up
+
+
+def check_for_natural(card1: tuple, card2: tuple):
+    if get_card_value(card1[0]) + get_card_value(card2[0]) == 21:
+        return True
+    else:
+        return False
+
+# todo is an ace considered an 11 or 1???
+# def get_hand_value(cards: list[namedtuple('card', ['value', 'suit'])]):
+
+
+def get_hand_value(cards):
+    sum = 0
+    for card in cards:
+        # get each card's "value"  #todo why can't I access using .value() from the named tuple?
+        sum += get_card_value(card[0])
+    return sum
+
+# https://stackoverflow.com/questions/41970795/what-is-the-best-way-to-create-a-deck-of-cards
+
+
+def create_deck():
+    card = namedtuple('card', ['value', 'suit'])
+    suits = ['hearts', 'diamonds', 'spades', 'clubs']
+    print("--------------------------------------------------------")
+    cards = [card(value, suit) for value in range(2, 15) for suit in suits]
+    print(cards)  # todo remove
+    print("--------------------------------------------------------")
+    deck = random.sample(cards, k=len(cards))  # shuffle the deck
+    print(deck)
+    print("--------------------------------------------------------")
+
+    return deck
+
+
+def main():
+
+    # todo create init deck function
+
+    deck = create_deck()
+    # todo what to do when deck is about to run out?
+
+    bankroll = 0
+    bankroll += add_to_bankroll(bankroll)
+    print(bankroll)
+
+    while (bankroll > 0):
+        print(f"Bankroll: {bankroll}")
+        bet = get_bet_amount(bankroll)
+        bankroll -= bet
+        player_cards = []
+        dealer_cards = []
+        # if deck has more than x cards left, else shuffle or end games?
+        player_cards.append(deck.pop())
+        dealer_cards.append(deck.pop())
+        print(
+            f"The dealer has the card {dealer_cards[0]} and another card face down")
+        player_cards.append(deck.pop())
+        dealer_cards.append(deck.pop())
+
+        print("Player cards: ", end="")
+        print(player_cards)
+        print("Dealer's cards: ", end="")
+        # todo change to  only print  one card (the face up card)
+        print(dealer_cards)
+
+        # todo get rid of check_for_natural function- it's not needed
+        if check_for_natural(player_cards[0], player_cards[1]):
+            print("Congratulations! You hit blackjack!")
+            if check_for_natural(dealer_cards[0], dealer_cards[1]):
+                print("The dealer also got blackjack! It's a push...")
+                bankroll += bet  # player gets their bet back
+            else:
+                print(f"You win 1.5x your bet which is: {bet*1.5}")
+                bankroll += bet*1.5
+        elif check_for_natural(dealer_cards[0], dealer_cards[1]):
+            print("The dealer hit blackjack!")
+
+        else:  # no one hit blackjack, continue...
+            # player gameplay
+            while (get_hand_value(player_cards) < 21):
+                print(f"Player's hand: {player_cards}")
+                # todo print gameplay options:
+                player_decision = input("What is your next move? ").lower()
+                if player_decision == "s":  # stand
+                    break  # exit, go on to dealer play
+                elif player_decision == "h":  # hit
+                    card = deck.pop()
+                    print(f"Your next card is {card}")
+                    player_cards.append(card)
+                else:
+                    print("Invalid input, try again...")
+            player_hand_value = get_hand_value(player_cards)  # store the value
+            if (player_hand_value > 21):
+                print("You went bust, dealer wins!")
+                bet = 0  # todo do I need this?
+                continue  # todo should I break out of this iteration differently?
+            elif (player_hand_value < 22):
+                print(
+                    f"Congrats! You hit {player_hand_value}, let's see how the dealer fairs...")
+
+            # dealer gameplay
+            print(f"The dealer's second card is: {dealer_cards[1]}")
+
+            # if dealer has a hand less than 17, they must hit
+            while get_hand_value(dealer_cards) < 17:
+                # If the dealer has an ace, and counting it as 11 would bring the total to 17 or more (but not over 21), the dealer must count the ace as 11 and stand.
+                card = deck.pop()
+                print(f"The dealer's next card is {card}")
+                dealer_cards.append(card)
+                print(dealer_cards)
+                print(
+                    f"The dealer's hand adds up to: {get_hand_value(dealer_cards)}")
+            if get_hand_value(dealer_cards) > 21:
+                print("The dealer went bust! You win!!!")
+                bankroll += bet * 1.5  # 3:2 payout
+                continue
+            # dealer check if they have 21
+            # todo create print function for human readable hand
+            print(dealer_cards)
+
+            if (get_hand_value(dealer_cards) > player_hand_value):
+                print("Dealer wins!")
+            elif (get_hand_value(dealer_cards) < player_hand_value):
+                print("Player wins")
+                bankroll += bet * 1.5  # 3:2 payout
+            else:  # push
+                bankroll += bet  # player get their bet back
+
+        bet = 0  # reset the player's bet... todo why do we need this code?
+        print(f"Player's bankroll is currently: {bankroll}")
+    print("Looks like you ran out of money... Maybe apply for a home equity or 401k loan?")
+
+
+if __name__ == "__main__":
+    # PythonApplication1.pymain()
+    main()
---
+++ test_pytest.py
@@ -0,0 +1,65 @@
+# There are 5 steps in the TDD flow:
+# Read, understand, and process the feature or bug request.
+# Translate the requirement by writing a unit test. ...
+# Write and implement the code that fulfills the requirement. ...
+# Clean up your code by refactoring.
+# Rinse, lather and repeat.
+
+from collections import namedtuple
+import blackjack
+import pytest
+
+
+# @pytest.mark.parameterize('x, result', [(2, 2), (3, 3), 4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10)])
+# @pytest.mark.parameterize("test_input,expected", [(2, 2)])
+# @pytest.mark.parametrize("test_input,expected", [(5, 5), (6, 6), (8, 9)])
+# @pytest.mark.parametrize("test_input,expected", [(2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10)])
+@pytest.mark.parametrize("test_input,expected", [(2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10),
+                                                 (10, 10), (11, 10), (12,
+                                                                      10), (13, 10), (14, 11),
+                                                 (0, ValueError), (-10, ValueError), (15, ValueError)])
+def test_getcardvalue(test_input, expected):
+    if type(expected) == type and issubclass(expected, Exception):
+        with pytest.raises(expected):
+            blackjack.get_card_value(test_input)
+    else:
+        assert blackjack.get_card_value(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 54)])
+def test_eval(test_input, expected):
+    assert eval(test_input) == expected
+
+
+def test_get_hand_value_17():
+    Card = namedtuple('Card', ['value', 'suit'])
+    # queen and 7 = 17
+    x = [Card(value=12, suit='clubs'), Card(value=7, suit='diamonds')]
+    assert blackjack.get_hand_value(x) == 17
+
+
+def test_get_hand_value_23():
+    Card = namedtuple('Card', ['value', 'suit'])
+    # queen, 7 and 6 = 23
+    x = [Card(value=12, suit='clubs'), Card(value=7, suit='diamonds'),
+         Card(value=6, suit='diamonds')]
+
+    assert blackjack.get_hand_value(x) == 23
+
+
+def test_get_hand_value_20():
+    card = namedtuple('Card', ['value', 'suit'])
+    card[0]
+    # king and jack = 20
+    x = [card(value=13, suit='clubs'), card(value=11, suit='diamonds')]
+
+    assert blackjack.get_hand_value(x) == 20
+
+
+def test_get_hand_value_blackjack():
+    card = namedtuple('Card', ['value', 'suit'])
+    card[0]
+    # king and ace = 21
+    x = [card(value=13, suit='clubs'), card(value=14, suit='diamonds')]
+
+    assert blackjack.get_hand_value(x) == 21
